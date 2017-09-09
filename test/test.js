0b748d622688849a97149407ddfe88dea53b859c
black-trooper <hermanmiller.dkr@gmail.com>
2017-09-09 16:59:39 +0900

    Change to Node.js format

--- test/test.js
+++ test/test.js
@@ -2,11 +2,11 @@ const assert = require('assert');
 const telSplitter = require('../src/index');
 
 describe('test-splitter', function () {
-  it('format', () => {
+  it('format', function () {
     assert.equal(telSplitter.format('031234xxxx'), '03-1234-xxxx')
   })
 
-  it('split', () => {
+  it('split', function () {
     assert.deepEqual(telSplitter.split('031234xxxx'), ['03', '1234', 'xxxx'])
   })
 })
